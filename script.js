ce2e0b7480ca5e8d49d588dad2fd35ca679dfb5f
Aurora <auduong.301@gmail.com>
2024-09-28 17:58:11 +0700

    complete category section

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ var swiper = new Swiper(".home-slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector(".header").classList.add("active");
+  } else {
+    document.querySelector(".header").classList.remove("active");
+  }
+};
